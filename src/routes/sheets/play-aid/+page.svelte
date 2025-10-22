60d80f6f21cfc81ed203c78dc422193492105da0
Iain Galloway <iain.galloway@gmail.com>
2025-10-22 11:47:08 +0100

    Add names to play aid

--- src/routes/sheets/play-aid/+page.svelte
+++ src/routes/sheets/play-aid/+page.svelte
@@ -1,83 +1,149 @@
 <h1 class="h1">Pendragon</h1>
 
-<section>
-  <h2 class="h2">Rolls</h2>
+<div class="grid grid-cols-1 gap-8 md:grid-cols-2 print:grid-cols-2">
+  <div>
+    <section>
+      <h2 class="h2">Rolls</h2>
 
-  <p>For an <strong>Unopposed Roll</strong>:</p>
+      <p>For an <strong>Unopposed Roll</strong>:</p>
 
-  <ul class="list-disc list-inside p-4">
-    <li>Establish your target value - the <strong>Chronicler</strong> will indicate which <strong>Skill</strong>, <strong>Passion</strong>, or <strong>Trait</strong> to use, and if any modifiers apply to it</li>
-    <li>Roll a d20 and compare the result to the target value:
-      <ul class="list-disc list-inside px-4">
-        <li>Result is a natural 20: <strong>Fumble</strong></li>
-        <li>Result is greater than the value: <strong>Failure</strong></li>
-        <li>Result exactly matches the value: <strong>Critical Success:</strong></li>
-        <li>Result is less than the value: <strong>Success:</strong></li>
+      <ul class="list-inside list-disc p-4">
+        <li>
+          Establish your target value - the <strong>Chronicler</strong> will indicate which
+          <strong>Skill</strong>, <strong>Passion</strong>, or <strong>Trait</strong> to use, and if
+          any modifiers apply to it
+        </li>
+        <li>
+          Roll a d20 and compare the result to the target value:
+          <ul class="list-inside list-disc px-4">
+            <li>Result is a natural 20: <strong>Fumble</strong></li>
+            <li>Result is greater than the value: <strong>Failure</strong></li>
+            <li>Result exactly matches the value: <strong>Critical Success:</strong></li>
+            <li>Result is less than the value: <strong>Success:</strong></li>
+          </ul>
+        </li>
+        <li>
+          If your modified skill value is below zero or above 20, increase the range for critical
+          success or fumbles
+        </li>
       </ul>
-    </li>
-    <li>If your modified skill value is below zero or above 20, increase the range for critical success or fumbles</li>
-  </ul>
 
-  <p>For a <strong>Opposed Roll</strong>:</p>
+      <p>For a <strong>Opposed Roll</strong>:</p>
 
-  <ul class="list-disc list-inside p-4">
-    <li>Each knight follows the process for an unopposed roll</li>
-    <li>Each knight that succeeds their own roll has <strong>Partial Success</strong></li>
-    <li>The knight that rolls the highest success is the <strong>Victor</strong></li>
-    <li>If multiple knights roll critical success, or identical successes, the result is a <strong>Tie</strong></li>
-  </ul>
-
-</section>
-
-<section>
-  <h2 class="h2">Passions and Inspiration</h2>
-
-  <p>For an <strong>Inspiration Roll</strong>:</p>
-
-  <ul class="list-disc list-inside p-4">
-    <li>Make an <strong>Unopposed Roll</strong> against the value of the <strong>Passion</strong></li>
-    <li>On a success, the knight is <strong>Inspired</strong> and gains +20 to rolls for the scene</li>
-    <li>On a failure, the knight is <strong>Disheartened</strong> and gains a -5 modifier to rolls for the scene</li>
-    <li>On a fumble, the knight is <strong>Maddened</strong> - give your character sheet to the <strong>Chronicler</strong></li>
-  </ul>
-
-</section>
-
-<section>
-  <h2 class="h2">Traits</h2>
-
-  <ul class="list-disc list-inside p-4">
-    <li>Traits can be used to help decide what action a knight should take.
-      <ul class="list-disc list-inside px-4">
-        <li>Make a choice based on free will, or</li>
-        <li>Make an <strong>Unopposed Roll</strong> using one of your traits, or</li>
-        <li>Make an <strong>Opposed Roll</strong> using two of your traits.</li>
+      <ul class="list-inside list-disc p-4">
+        <li>Each knight follows the process for an unopposed roll</li>
+        <li>Each knight that succeeds their own roll has <strong>Partial Success</strong></li>
+        <li>The knight that rolls the highest success is the <strong>Victor</strong></li>
+        <li>
+          If multiple knights roll critical success, or identical successes, the result is a <strong
+            >Tie</strong
+          >
+        </li>
       </ul>
-    </li>
-    <li>A trait with a value of <strong>16</strong> or higher is a <strong>Famous Trait</strong>. The <strong>Chronicler</strong> may call for a trait roll if a knight wishes to act against it
-  </ul>
-</section>
+    </section>
 
-<section>
-  <h2 class="h2">Combat</h2>
+    <section>
+      <h2 class="h2">Passions and Inspiration</h2>
 
-  <ul class="list-disc list-inside p-4">
-    <li>Each knight has a list of <strong>Combat Skills</strong> they are trained in</li>
-    <li>When in combat, the <strong>Chronicler</strong> will call for an <strong>Opposed Roll</strong> using the relevant <strong>Combat Skill</strong></li>
-    <li>The victor deals damage to the loser based on their weapon:
-      <ul class="list-disc list-inside px-4">
-        <li><strong>Sword:</strong> 4d6</li>
-        <li><strong>Laser Lance Charge:</strong> 5d6</li>
+      <p>For an <strong>Inspiration Roll</strong>:</p>
+
+      <ul class="list-inside list-disc p-4">
+        <li>
+          Make an <strong>Unopposed Roll</strong> against the value of the <strong>Passion</strong>
+        </li>
+        <li>
+          On a success, the knight is <strong>Inspired</strong> and gains +20 to rolls for the scene
+        </li>
+        <li>
+          On a failure, the knight is <strong>Disheartened</strong> and gains a -5 modifier to rolls
+          for the scene
+        </li>
+        <li>
+          On a fumble, the knight is <strong>Maddened</strong> - give your character sheet to the
+          <strong>Chronicler</strong>
+        </li>
       </ul>
-    </li>
-    <li>Critical successes deal double damage (e.g. 8d6 for a Sword)</li>
-    <li>Subtract the defender's armour:
-      <ul class="list-disc list-inside px-4">
-        <li><strong>Light Armour:</strong> -2 damage</li>
-        <li><strong>Medium Armour:</strong> -4 damage</li>
-        <li><strong>Heavy Armour:</strong> -10 damage</li>
-        <li><strong>Shield:</strong> -6 damage on a <strong>Partial Success</strong></li>
+    </section>
+
+    <section>
+      <h2 class="h2">Traits</h2>
+
+      <ul class="list-inside list-disc p-4">
+        <li>
+          Traits can be used to help decide what action a knight should take.
+          <ul class="list-inside list-disc px-4">
+            <li>Make a choice based on free will, or</li>
+            <li>Make an <strong>Unopposed Roll</strong> using one of your traits, or</li>
+            <li>Make an <strong>Opposed Roll</strong> using two of your traits.</li>
+          </ul>
+        </li>
+        <li>
+          A trait with a value of <strong>16</strong> or higher is a <strong>Famous Trait</strong>.
+          The
+          <strong>Chronicler</strong> may call for a trait roll if a knight wishes to act against it
+        </li>
       </ul>
-    </li>
-  </ul>
-</section>
+    </section>
+  </div>
+
+  <div>
+    <section>
+      <h2 class="h2">Combat</h2>
+
+      <ul class="list-inside list-disc p-4">
+        <li>Each knight has a list of <strong>Combat Skills</strong> they are trained in</li>
+        <li>
+          When in combat, the <strong>Chronicler</strong> will call for an
+          <strong>Opposed Roll</strong>
+          using the relevant <strong>Combat Skill</strong>
+        </li>
+        <li>
+          The victor deals damage to the loser based on their weapon:
+          <ul class="list-inside list-disc px-4">
+            <li><strong>Sword:</strong> 4d6</li>
+            <li><strong>Laser Lance Charge:</strong> 5d6</li>
+          </ul>
+        </li>
+        <li>Critical successes deal double damage (e.g. 8d6 for a Sword)</li>
+        <li>
+          Subtract the defender's armour:
+          <ul class="list-inside list-disc px-4">
+            <li><strong>Light Armour:</strong> -2 damage</li>
+            <li><strong>Medium Armour:</strong> -4 damage</li>
+            <li><strong>Heavy Armour:</strong> -10 damage</li>
+            <li><strong>Shield:</strong> -6 damage on a <strong>Partial Success</strong></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+
+    <section>
+      <h1 class="h2">Names</h1>
+
+      <h2 class="h3">Male Names</h2>
+      <p>
+        Adtherp, Alarent, Alein, Aliduke, Annecians, Archade, Arnold, Arrouse, Bandelaine,
+        Bastianor, Bellangere, Bellias, Berel, Bersules, Bliant, Breunis, Briant, Caulas, Cendrius,
+        Chestelaine, Clegis, Cleremond, Dalan, Darnel, Dinaunt, Driant, Ebel, Edward, Eldovar,
+        Elias, Eliot, Emerause, Fendrin, Flannedrius, Florence, Floridas, Galardoun, Garnish,
+        Gauter, Gawenor, Gerin, Gherard, Gilbert, Gilmere, Goneries, Gracian, Gumret, Guy, Gwinas,
+        Harsouse, Harvis, Hebes, Hemison, Herawd, Heringdale, Herlews, Hermel, Hermind, Hervis,
+        Hewgon, Idres, Jordans, Lardans, Leomie, Lucarien, Manasan, Maurel, Melion, Merival, Miles,
+        Morganor, Morians, Moris, Nanowne, Nerovens, Pedivere, Pellandres, Pellogres, Perin, Phelot,
+        Pillounes, Plaine, Plenorias, Sauseise, Selises, Selivant, Semond, Tharanis.
+      </p>
+
+      <h2 class="h3">Female Names</h2>
+      <p>
+        Ade, Alis, Arnive, Astrigis, Aveline, Belisant, Bene, Blancheflor, Carsenefi, Clariette,
+        Cunneware, Delyse, Diane, Eirene, Elidis, Elizabeth, Enide, Esclarmonde, Esmere, Falisse,
+        Feimurgan, Felelolie, Felinete, Feunete, Florie, Galienne, Gloris, Gweneira, Heliap,
+        Helisande, Iblis, Idain, Imane, Irelisse, Ismena, Jeschute, Jolette, Laudine, Laufamour,
+        Liaze, Loorette, Lore, Lysandra, Malvis, Maugalie, Melior, Meraude, Morchades, Naevine,
+        Obie, Obilot, Olwenne, Oriane, Oruale, Palisse, Persevia, Quenilda, Repanse, Revanne,
+        Roselune, Sabine, Sangive, Seliora, Silvette, Syrane, Talwen, Tanree, Thalissa, Tryamour,
+        Vervaine, Violette, Vivane, Ysoria, Zephra.
+      </p>
+    </section>
+  </div>
+</div>
