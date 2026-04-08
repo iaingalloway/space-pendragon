c660fce8e390ad1ffd321fcdaadde11eac34ff14
Iain Galloway <iain.galloway@gmail.com>
2026-04-08 16:56:07 +0100

    Flesh out Uther's court

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,18 +12,24 @@
 <ul class="list-disc list-inside p-4">
   <li>Roderick's court:
     <ul class="list-disc list-inside px-4">
-      <li><a href="/characters/roderick" target="_blank">Roderick, Earl of Sarum</a></li>
-      <li>Jensen, the Earl's son</li>
+      <li><a href="/characters/roderick" target="_blank">Roderick, Earl of Sarum (Bear)</a></li>
+      <li>Jensen, the Earl's son (Bear)</li>
       <li>Ellen, Countess of Sarum</li>
-      <li><a href="/characters/elad" target="_blank">Elad, Marshal of Sarum</a></li>
+      <li><a href="/characters/elad" target="_blank">Elad, Marshal of Sarum (Badger)</a></li>
     </ul>
   </li>
 
   <li>Uther's court:
     <ul class="list-disc list-inside px-4">
-      <li><a href="/characters/arfius" target="_blank">Arfius</a></li>
-      <li><a href="/characters/baastias" target="_blank">Baastias</a></li>
-      <li><a href="/characters/madoc" target="_blank">Prince Madoc</a></li>
+      <li>King Uther Pendragon (Lion)</li>
+      <li><a href="/characters/arfius" target="_blank">Marshal Arfius, Duke of Silchester (Dog)</a></li>
+      <li><a href="/characters/baastias" target="_blank">Sir Baastias, Uther's Bodyguard (Sheep)</a></li>
+      <li><a href="/characters/madoc" target="_blank">Prince Madoc (Lion)</a></li>
+
+      <li>Gorlois, Duke of Cornwall (Wildcat)</li>
+      <li>Lady Ygraine (Swan)</li>
+
+      <li>Eadric, Duke of Lindsey (Wolf)</li>
     </ul>
   </li>
 </ul>
