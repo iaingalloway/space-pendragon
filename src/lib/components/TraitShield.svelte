555e19c546b3611f38e6f635376ea6e17fe47cf4
Iain Galloway <iain.galloway@gmail.com>
2026-09-16 12:53:08 +0000

    Add glitch effect to famous trait shields

--- src/lib/components/TraitShield.svelte
+++ src/lib/components/TraitShield.svelte
@@ -10,6 +10,7 @@
   <svg
     viewBox={traitShieldViewBox}
     class:famous={isFamous}
+    class:trait-shield-glitch={isFamous}
     class="trait-shield h-14 w-12"
     xmlns="http://www.w3.org/2000/svg"
     stroke-width="4"
@@ -17,8 +18,22 @@
     <path d={traitShieldPath} />
   </svg>
 
+  {#if isFamous}
+    <svg
+      viewBox={traitShieldViewBox}
+      class:famous={isFamous}
+      class="trait-shield trait-shield-glitch-slice h-14 w-12"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke-width="4"
+      aria-hidden="true"
+    >
+      <path d={traitShieldPath} />
+    </svg>
+  {/if}
+
   <span
     class:famous={isFamous}
+    class:trait-shield-glitch-value={isFamous}
     class="trait-shield-value absolute inset-0 flex items-center justify-center pb-1 text-[1.1rem] font-bold"
   >
     {value ?? '\u00A0'}
